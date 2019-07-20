8d8d1fc948438d2b7c3c8fbd72b50cdc040c6971
Jacob Richards <jacob.richards33@gmail.com>
2019-07-19 17:39:54 -0700

    command line option for specifying config filepath

--- index.ts
+++ index.ts
@@ -6,15 +6,37 @@ import applicationConfig from './src/applicationConfig';
 const ora = require('ora');
 const fs = require('fs');
 
-fs.readFile(`${process.cwd()}/preducksConfig.json`, (err, data) => {
-  if (err){
-    console.log('error reading preducksConfig: ',err);
-  }
-  else{
-    const preducksConfig = JSON.parse(data);
-    createPreducksApp(preducksConfig.appName, preducksConfig);
-  }
-})
+let [,,configFilePath] = process.argv;
+
+const defaultPreducksConfig = {
+  interfaces: {},
+  reducers: {
+    DEFAULT: {
+      store: {},
+      actions: {},
+    },
+  },
+};
+
+if(configFilePath){
+  configFilePath = configFilePath[0] ==='.' ? configFilePath.slice(2) : configFilePath;
+}
+const configPath = configFilePath ? `${process.cwd()}/` + configFilePath : `${process.cwd()}/preducksConfig.json`;
+
+if(fs.existsSync(configPath)){
+  fs.readFile(configPath, (err, data) => {
+    if (err){
+      console.log("\x1b[31m",'error reading preducksConfig: ',err);
+    }
+    else{
+      const preducksConfig = JSON.parse(data);
+      createPreducksApp(preducksConfig.appName, preducksConfig);
+    }
+  })
+} else {
+  console.log("\x1b[35m",'  ❗️   no preducksConfig.json found. cooking up a default preducks app ✨', "\x1b[0m")
+  createPreducksApp('preducksApp', defaultPreducksConfig);
+}
 
 function createPreducksApp(appName, preducksConfig){
   createApplicationUtil(
@@ -33,16 +55,16 @@ function createPreducksApp(appName, preducksConfig){
 
 
 const throbber = ora({
-  text: 'building preducks app',
+  text: "\x1b[36m" +'building preducks app',
   spinner: {
-    frames: ['  🥚  ','  🐣  ','  🐤  ','  🐥  '],
+    frames: ['   🥚  ','   🐣  ','   🐤  ','   🐥  '],
     interval: 250, // Optional
   },
 }).start();
 
 setTimeout(() => {
   throbber.stopAndPersist({
-    symbol: '  🦆  ',
-    text: 'finished building preducks app',
+    symbol: '   🦆  ',
+    text: "\x1b[42m" + "\x1b[30m" + 'finished building preducks app' + "\x1b[0m" + '  🦆  ',
   });
 }, 1000);
